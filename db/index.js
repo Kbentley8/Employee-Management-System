0c38fa52a41f463318cda6b3bad43e6f27038dfe
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-12 00:26:35 -0400

    edited index.js

--- db/index.js
+++ db/index.js
@@ -2,16 +2,16 @@ const connection = require("./connection");
 
 class employeeDB {
 
-     this.connection = connection;
-}
+    constructor(connection) {
+        this.connection = connection;
+    }
 
-
-// show all employees
-allEmployees() {
-    return this.connection.promise().query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-    );
-}
+    // Show all employees
+    allEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
 
 // Add an employee
 addEmployee(employee) {
