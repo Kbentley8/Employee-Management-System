99455d3ac101b879aa7ca9ea473b340a093f9782
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-12 14:36:22 -0400

    edited schema.sql and seeds.sql, edited index to server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,8 @@
-use employees;
+INSERT INTO department(department_name)
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal"), ("Marketing");
 
-INSERT INTO department
-    (name)
-VALUES
-    ('Human Resources'),
-    ('Marketing'),
-    ('Information Technology'),
-    ('Accounting');
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("Chief Counsel", 300000, 4);
 
-INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('HR Manager', 100000, 1),
-    ('HR', 50000, 1),
-    ('Marketing Director', 140000, 2),
-    ('Sales', 65000, 2),
-    ('IT Director', 195000, 3),
-    ('Desktop Support', 100000, 3),
-    ('Account Manager', 110000, 4),
-    ('Accountant', 90000, 4);
-
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-VALUES
-    ('Jackie', 'Petit', 1, NULL),
-    ('Max', 'Gonzales', 2, 1),
-    ('Calvin', 'Kim', 3, NULL),
-    ('Yukata', 'Mikoto', 4, 3),
-    ('Maria', 'Longwood', 4, 3),
-    ('Sheri', 'Green', 5, NULL),
-    ('Jonathan', 'Prune', 6, 5),
-    ('Andrew', 'Romans', 6, 5),
-    ('Julius', 'Thomas', 7, NULL),
-    ('Sari', 'Rasa', 7, 8);    
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Johnnie', 'Random', 1, 2), ('James', 'Smith', 1, null), ('Ronnie', 'Manning', 1, 2), ('Jimmy', 'Jones', 2, 2), ('Larry', 'Legal', 4, null);
