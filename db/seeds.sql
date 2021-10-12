07bdc3b0e7d6ce1873cb1461947c2b356cf5d805
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-12 15:19:40 -0400

    edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,38 @@
-INSERT INTO department(department_name)
-VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal"), ("Marketing");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Sott', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jim', 'Halpert', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dwight', 'Schrute', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Pam', 'Beasley', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kevin', 'Malone', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Toby', 'Flannagan', 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Robert', 'California', 6, null);
 
-INSERT INTO role(title, salary, department_id)
-VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("Chief Counsel", 300000, 4);
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Sales');
+INSERT INTO department (department_name)
+VALUES ('Accounting');
+INSERT INTO department (department_name)
+VALUES ('Reception');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Johnnie', 'Random', 1, 2), ('James', 'Smith', 1, null), ('Ronnie', 'Manning', 1, 2), ('Jimmy', 'Jones', 2, 2), ('Larry', 'Legal', 4, null);
+INSERT INTO role (title, salary, department_id)
+VALUES ('General Manager', 120000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesman', 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 90000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Receptionist', 40000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Human Resource Officer', 75000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ('CEO', 250000, null);
